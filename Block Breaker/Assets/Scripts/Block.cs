e866a10afb1f2d7e726743453d6327cc8349c5ec
Keenan <keenan.carroll.va@gmail.com>
2019-03-16 12:34:38 -0400

    getting score to carry over

--- Block Breaker/Assets/Scripts/Block.cs
+++ Block Breaker/Assets/Scripts/Block.cs
@@ -17,6 +17,14 @@ public class Block : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        DestroyBlock();
+
+    }
+
+    private void DestroyBlock()
+    {
+        FindObjectOfType<GameStatus>().AddToScore();
+
         AudioSource.PlayClipAtPoint(blockBreak, Camera.main.transform.position);
         Destroy(gameObject);
         level.BlocksDestroyed();
---
+++ Block Breaker/Assets/Scripts/GameStatus.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameStatus : MonoBehaviour
+{
+    // config params
+    [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
+    [SerializeField] int pointsPerBlock = 1;
+    [SerializeField] TextMeshProUGUI scoreText;
+
+    // state
+    [SerializeField] int currentScore = 0;
+
+    private void Awake()
+    {
+        int gameStatusCount = FindObjectsOfType<GameStatus>().Length;
+        if (gameStatusCount > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = currentScore.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale = gameSpeed;
+    }
+
+    public void AddToScore()
+    {
+        currentScore += pointsPerBlock;
+        scoreText.text = currentScore.ToString();
+    }
+}
